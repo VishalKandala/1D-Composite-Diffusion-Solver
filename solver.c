1479228acfad07c7e03cdafdef9d7db39dfecc7c
VishalKandala <vishalkandala@tamu.edu>
2022-11-22 15:02:53 -0600

    first commit

---
+++ solver.c
@@ -0,0 +1,13 @@
+// Vishal Indivar Kandala
+// MEEN 689 Computing Concepts Project 3
+
+#include <stdlib.h>
+#include "init.c"
+// 
+// A in:  A*u(n+1) = B
+void formA(**A);
+// B in:  A*u(n+1) = B
+void formB(*B);
+
+
+
