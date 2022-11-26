b5fa94876b9cff659488e0c61a6e9b46871a3193
VishalKandala <vishalkandala@tamu.edu>
2022-11-26 04:12:56 -0600

    Useful comments added to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,22 @@
 #include "init.hpp"
 using namespace std;
 int main(int argc, char **argv){
-    // v for verbose: when v = 1, basic info
-    // v = 2
-//    using namespace heat;
+    int v = stoi(argv[3]);  
+    // v for verbose: when v = 1, basic info.
+    // v = 2, A matrix.
+    // v = 3 , B vector at every time step.
 
-    int temp = stoi(argv[1]); 
-    double temp2 = stod(argv[2]);
-    double temp3 = stod(argv[3]);
-    int v = stoi(argv[3]);
+    // Reading command line inputs.	
+    int N = stoi(argv[1]); // Command line input for Number of nodes in radial direction.  
+    double dt = stod(argv[2]); // Command line input for time step size. 
+    double ft = stod(argv[3]); // Command line input for simulation interval.
     int layup;
     layup = 1;
-    //double re = dr();
-    //cout << temp2 << endl;
-    heat::Define_Vars(temp,temp2,temp3);
+    // Define global variables using inputs.
+    heat::Define_Vars(N,dt,ft);
     //-------------------
     if(v==1){
+    // Print out basic info when v = 1
     cout << "Number of Nodes: "<<heat::Num_of_nodes << endl;
     cout << "Grid size: "<<heat::dr << endl;
     cout<<"Time step: "<<heat::dt<<endl;
@@ -28,7 +29,9 @@ int main(int argc, char **argv){
     //-------------------
     // Time loop
     for(int it = 0;it<heat::Nt;it++){
+    // Advance the solution by one Timestep(dt)	    
     heat::Advance_dt(layup,v);
+    // Update solution monitor time t.
     heat::t+=heat::dt;
     cout<<"t = "<<heat::t<<endl;
     }
