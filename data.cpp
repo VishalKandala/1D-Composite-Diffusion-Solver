0a03eebd846476c51da319420a963c43cc909393
Thomas Firsich <thomasfirsich@yahoo.com>
2022-11-29 02:21:31 +0000

    Replace data.cpp.
    FIXING THE MAIN BRANCH - THOMAS

--- data.cpp
+++ data.cpp
@@ -1,93 +1,11 @@
 //Vishal Indivar Kandala
 // MEEN 689 Computing Concepts Project 3
-// Edited by: Thomas Firsich
-// Last Edited: 11/28
-
 #include<iostream>
 #include<fstream>
 #include<sstream>
 #include "heat.hpp"
 
 using namespace std;
-
-// This file is used to read an input file for the user to run the solver
-// Please follow the structure of the file to ensure the code runs as intended!!
-void heat::File_Read(inf* info, string filename){
-	//Verbose meaning how much you want to know about your output
-	int v;
-	//N for grid size
-	int N;
-	//dt for time step size
-	double dt;
-	//ft for simulation interval
-	double ft;
-	//outfreq for frequency to output to a file for post-processing
-	int outfreq;
-
-
-	//Initialize a string variable for the line that will be read
-	string line;
-	//open the file
-	ifstream myfile (filename);
-	
-	if (myfile.is_open()){
-		//Get the first line
-		getline(myfile, line);
-		//Calculate the number of columsn, which should equal the number of variables assigned for the solver
-		int count = 0;
-		for(int i = 0; i < line.size(); i++){
-			if(line[i] == ','){
-				count = count + 1;
-			}
-		}
-		//Number of columns is more than the number of commas
-		count = count + 1;
-		info->count = count;
-		//Reset the ifstream back to the first line
-		myfile.clear()
-		myfile.seekg(0);
-		//Loop through the columns of the first row to read the variables in the header
-		getline(myfile,line);
-		stringstream tempstring1(line);
-		for (int i = 0; i < count; i++){
-			string temp;
-			getline(tempstring1, temp, ',');
-			info->headers.push_back(temp);
-		}
-		//Loop through the columns of the second row for assigning the correct variables
-		getline(myfile, line);
-		stringstream tempstring2(line);
-		for (int i = 0; i < info->count; i++){
-			string temp2;
-			getline(tempstring2, temp2, ',');
-			info->vals.push_back(temp2);
-		}
-		//Now that we have the array of headers, assign the variables accordingly 
-		for (int i = 0; i <= (info->count); i++;){
-			if (info->headers[i] == "v"){
-				v = info->vals[i];
-			} 
-			else if(info->headers[i] == "N"){
-				N = info->vals[i];
-			}
-			else if(info->headers[i] == "dt"){
-				dt = info->vals[i];
-			}
-			else if(info->headers[i] == "ft"){
-				ft = info->vals[i];
-			}
-			else if(info->headers[i] == "outfreq"){
-				outfreq = info->vals[i];
-			}
-			//else if(info->headers[i] == "layup"){
-			//	layup = info->vals[i];
-			//}
-			else{
-				cout << "Error reading in the solver variabels from file " << filename << endl;
-			}
-		}
-	}
-}
 
 void heat::Print_File(int it,int interactive){
 	ofstream outputfile;
